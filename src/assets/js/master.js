24e2694086b20d5ca3d79206e30479d37838a2b8
yasamanm mirzaei <yasamanmirzaei1376@gmail.com>
2026-02-27 20:36:42 +0330

    Add files via upload

---
+++ src/assets/js/master.js
@@ -0,0 +1,28 @@
+const originInput = document.getElementById("originInput")
+const originMenu = document.getElementById("originMenu")
+const destinationInput = document.getElementById("destinationInput")
+const destinationMenu = document.getElementById("destinationMenu")
+
+originInput.addEventListener("click", function (e) {
+    e.stopPropagation()
+    originMenu.classList.toggle("hidden")
+    destinationMenu.classList.add("hidden")
+})
+
+destinationInput.addEventListener("click", function(e){
+   e.stopPropagation()
+   destinationMenu.classList.toggle("hidden")
+   originMenu.classList.add("hidden")
+})
+
+document.addEventListener("click", function () {
+    originMenu.classList.add("hidden")
+    destinationMenu.classList.add("hidden")
+})
+originMenu.addEventListener("click", function(e){
+    e.stopPropagation()
+})
+
+destinationMenu.addEventListener("click", function(e){
+    e.stopPropagation()
+})
